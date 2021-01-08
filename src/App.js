d869f79f85df5df43949a7eacebe8b86154f1e10
Annie Chernich <anniechernich@AC-MacBook-Air.local>
2021-01-08 13:19:27 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+const name = 'Annie';
+const hobbies = ['running', 'reading', 'cooking'];
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className= "aboutAnnie"><h1 id = "myName">Hello my name is {name}</h1>
+    <p id= "myHobbies">My hobbies include:
+      <ul>
+        <li>{hobbies [0]}</li>
+        <li>{hobbies[1]}</li>
+        <li>{hobbies[2]}</li>
+      </ul>
+    </p>
+    <p id= "funFact">My fun fact is that I can speak French fluently!</p>
     </div>
   );
 }
